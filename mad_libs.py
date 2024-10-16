572ddf89274593bc03ec6ac1072da0fcc5b02ce3
NoahSonneKargo <143974142+NoahSonneKargo@users.noreply.github.com>
2024-10-16 15:04:31 -0400

    Add files via upload
    Created a mad libs game with python coding

---
+++ mad_libs.py
@@ -0,0 +1,41 @@
+# Noah Sonne Kargo
+
+# My story comes from the Religion class I have. In the class we read "I have a dream" by Marting Luther King.
+# In class I found it kind of boring and thought it would be funny to take a few lines and make it into a game of Mad Libs.
+
+print("Let's play Mad Libs")
+print("Please go ahead and fill out the parts of speech you will be given.")
+
+# Words that needs to be filled out
+
+word_one = input("Enter a noun: ")
+word_two = input("Enter a place: ")
+word_three = input("Enter a adjective: ")
+word_four = input("Enter a plural noun: ")
+word_five = input("Enter a noun: ")
+word_six = input("Enter a body part: ")
+word_seven = input("Enter a noun: ")
+word_eight = input("Enter a noun: ")
+word_nine = input("Enter a place: ")
+word_ten = input("Enter a adjective: ")
+word_eleven = input("Enter a adjective: ")
+word_twelve = input("Enter a adjective: ")
+word_thirteen = input("Enter a adjective: ")
+word_fourteen = input("Enter a noun: ")
+
+print("Your final story is the following:")
+
+#Print chosen text with the inserted words
+
+print(f"I have a {word_one} that one day down in {word_two}, with its {word_three} {word_four}, with its {word_five} ")
+print(f"having his {word_six} dripping with the words of “{word_seven}” and “{word_eight},” one day right there in ")
+print(f"{word_nine} little {word_ten} boys and {word_eleven} girls will be able to join hands with little ")
+print(f"{word_twelve} boys and {word_thirteen} girls as sisters and brothers. I have a {word_fourteen} today.")
+
+print("Thank you for playing!")
+
+
+
+
+
+
